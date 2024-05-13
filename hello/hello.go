dbc3dab337373894026f9efb8ba257dac082b5f6
Sieep-Coding <nstamb6@gmail.com>
2024-05-13 05:20:41 -0700

    learning about map

--- hello/hello.go
+++ hello/hello.go
@@ -11,12 +11,14 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Nick")
+	names := []string{"Nick", "Sammy", "Gerrit"}
+
+	messages, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 	// Get a greeting message and print it.
 	// message := greetings.Hello("Gladys")
-	fmt.Println(message)
+	fmt.Println(messages)
 }
